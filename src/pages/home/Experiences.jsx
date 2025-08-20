942e93ff57cd95f0ec435036899a189c45f8e0eb
mhmasum1 <mhmasum231@gmail.com>
2025-08-20 13:22:12 +0600

    Experience section added and make it responsive

---
+++ src/pages/home/Experiences.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import experienceImg from '../../assets/expricences.png'
+import Button from '../../components/Button'
+
+const Experiences = () => {
+    return (
+        <section className='max-w-screen-2xl container mx-auto my-24 flex flex-col md:flex-row items-center justify-between md:gap-20 gap-8 '>
+            <div className=' md:w-1/2 md:h-[541px]'>
+                <img className='h-full w-full' src={experienceImg} alt="" />
+            </div>
+            <div className=' md:w-1/2 mx-auto'>
+                <h3 className=' uppercase text-lg font-semibold text-primary mb-4'>experiences</h3>
+                <h2 className=' capitalize text-4xl font-bold mb-4 md:w-1/2'> we provide you the best experience</h2>
+                <p className=' text-black dark:text-white mb-5 lg:w-2/3'>You don’t have to worry about the result because all of these interiors are made by people who are professionals in their fields with an elegant and lucurious style and with premium quality materials</p>
+                <Button></Button>
+            </div>
+        </section>
+    )
+}
+
+export default Experiences
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import WhyChoosingUs from './WhyChoosingUs'
 import Products from '../shop/Products'
+import Experiences from './Experiences'
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Hero></Hero>
             <WhyChoosingUs></WhyChoosingUs>
             <Products headline="Best Selling Products"></Products>
+            <Experiences></Experiences>
 
         </>
     )
