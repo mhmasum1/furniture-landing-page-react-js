446203d450bbbf1c6285116b69cfa0726f1167c6
mhmasum1 <mhmasum231@gmail.com>
2025-08-21 02:12:04 +0600

    Added contact and about section

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import contactBgImg from "../../assets/contact-background.jpg"
+import Testimonials from '../home/Testimonials'
+import Matarials from '../home/Matarials'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <section>
+            {/* banner */}
+            <div
+                className="w-full h-[400px]  bg-no-repeat bg-cover bg-center   flex items-center justify-center text-white"
+                style={{ backgroundImage: `url(${contactBgImg})` }}
+            >
+                <div className=''>
+                    <h1 className="text-5xl font-bold">Contact</h1>
+                </div>
+            </div>
+            <Matarials></Matarials>
+            <Testimonials />
+        </section>
     )
 }
 
